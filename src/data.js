400b4e4c66fa6edf96dee421f545f4992e805088
RafaPessoa <90880251+PessoaRafael@users.noreply.github.com>
2022-01-23 20:29:29 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import img1 from './img1.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: ` Banana Fruta  
+            Nutrientes:
+
+                    
+            Proteinas: 0
+            Carboidratos: 5.5
+
+            Gordura(s): 0.4
+            Calorias: 29
+            Açúcar: 5.4`
+
+
+            ,
+            desc: '',
+            price: 46,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: `Morango
+            Nutrientes:
+
+                    
+            Proteinas: 0
+            Carboidratos: 5.5
+
+            Gordura(s): 0.4
+            Calorias: 29
+            Açúcar: 5.4`,
+            desc: '',
+            price: 48,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: `Pera Fruta
+            Nutrientes:
+
+                    
+            Proteinas: 0
+            Carboidratos: 5.5
+
+            Gordura(s): 0.4
+            Calorias: 29
+            Açúcar: 5.4`,
+            desc: '',
+            price: 46,
+        },
+    
+     
+    ],
+};
+
+export default data;
